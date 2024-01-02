e574d990e80fe0b961363fe69bd5354f633adc8f
Nicolas BERNA <nicolas_github@berna.fr>
2024-01-02 22:03:46 +0100

    fix mouting tun0

--- config/ovpn-start.sh
+++ config/ovpn-start.sh
@@ -3,10 +3,10 @@ set -e
 
 
 
-#mkdir -p /dev/net
-#if [ ! -c /dev/net/tun ]; then
-#	mknod /dev/net/tun c 10 200
-#fi
+mkdir -p /dev/net
+if [ ! -c /dev/net/tun ]; then
+    mknod /dev/net/tun c 10 200
+fi
 #iptables -t nat -A POSTROUTING -j MASQUERADE
 #iptables -A FORWARD -i tun0 -o tun0 -j DROP
 exec openvpn --config config.conf
